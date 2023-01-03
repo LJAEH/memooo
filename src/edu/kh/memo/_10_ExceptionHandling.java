d129ea5a40a78f2e018e34a1c4223881f7b8c55b
LJAEH <wogurdlek33@gmail.com>
2023-01-03 14:21:51 +0900

    0103

--- src/edu/kh/memo/_10_ExceptionHandling.java
+++ src/edu/kh/memo/_10_ExceptionHandling.java
@@ -15,7 +15,7 @@ public class _10_ExceptionHandling {
 Unchecked Exception = 확인 굳이 안해도 되는 예외 : 선택적 예외처리
  => Runtime Exception => ...
 
-Checked Exception = 확인을 꼭 해야만하는 예이 : 예외처리 필수
+Checked Exception = 확인을 꼭 해야만하는 예외 : 예외처리 필수
 
 
 
@@ -31,8 +31,31 @@ System.exit();를 만나면 무조건 프로그램 종료
 주로 java.io나 java.sql 패키지의 메소드 처리 시 이용
 
 
+// 런타임 에러 : 프로그램 수행 중 발생하는 에러
+		//			 주로 if문으로 처리 가능
+		
+		int[] arr = new int[-1]; // 길이 3: 인덱스 0,1,2까지
+		
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		
+		if( 3 >= arr.length ) {
+			System.out.println("배열 범위를 초과했습니다.");
+		} else {
+			arr[3] = 40;			
+		}
+		
+		
+		// ArrayIndexOutOfBoundsException: 배열 범위 초과 예외
+		//  ==> 배열에서 부적절한 인덱스에 접근하려고 할 때 발생될 수 있는 예외
 
+		// NegativeArraySizeException : 배열이 음수의 크기일때 발생 예외
+		// 배열의 크기로 음수를 제시했을 때 발생될 수 있는 예외
+		
 
+		// ********* <== 배열 예외처리 잘 알아 두기 **********
+	}
 
 
 
